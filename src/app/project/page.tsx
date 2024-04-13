7591bfcd13c019ac7d2e4c89ff28f376d259cf59
kelvin cosmas <emcldeveloper@gmail.com>
2024-04-13 17:57:44 +0300

    use map to get data

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,6 +1,64 @@
+'use client'
 import kevi from "../../../public/image/kevi.jpg";
 import Image from "next/image";
+import { FaArrowAltCircleRight } from "react-icons/fa";
+import { useState } from "react";
 const Project=()=>{
+
+    const [openModalForm, setModael]=useState(false);
+
+    const project = [
+        {
+          id: 1,
+          title:"Ekazi",
+          subtitle:"ekazi design",
+          link: "home",
+          imag:kevi
+
+        },
+        {
+            id: 2,
+            title:"Ekazi",
+            subtitle:"ekazi design",
+            link: "home",
+            imag:kevi
+        },
+        {
+            id: 3,
+            title:"Ekazi",
+            subtitle:"ekazi design",
+            link: "home",
+            imag:kevi
+        },
+        {
+            id: 4,
+            title:"Ekazi",
+            subtitle:"ekazi design",
+            link: "home",
+            imag:kevi
+        },
+        {
+            id: 5,
+            title:"Ekazi",
+            subtitle:"ekazi design",
+            link: "home",
+            imag:kevi
+        },
+      ];
+
+      const handleProject=()=>{
+
+        alert ("hello Is under design")
+
+      }
+
+      const openModal=()=>{
+
+        return(
+
+            <><p>Hello I'm Mr Modal Do you want to Open Me</p></>
+        )
+      }
 
     return (
 
@@ -18,32 +76,36 @@ const Project=()=>{
                </div>
            </div>
            <div className="flex flex-wrap md:px-40 flex-row px-2 ">
-  <div className="w-full md:w-1/3 p-1">
-   
-    <div className=" rounded-lg bg-white  thrink-0">
-    <Image src={kevi}  alt="project" className="w-[100%] p-2 md:hover:scale-95"/>
-    <p className="font-bold px-2">Web Design</p>
-     <p className="text-slate-500 px-2">Web Design</p>
-    </div>
-     
-  </div>
-  <div className=" w-full  md:w-1/3 p-1">
-  <div className="rounded-lg bg-white w-full   thrink-0">
-  <Image src={kevi} alt="project" className="w-[100%] p-2 md:hover:scale-95"/>
-  <p className="font-bold px-2">Web Design</p>
-     <p className="text-slate-500 px-2">Web Design</p>
-  </div>
-  </div>
-  <div className=" w-full  md:w-1/3 p-1">
-  <div className="rounded-lg bg-white w-full  thrink-0 ">
-  <Image src={kevi} alt="project" className="w-[100%] p-2 md:hover:scale-95"/>
-  <p className="font-bold px-2">Web Design</p>
-     <p className="text-slate-500 px-2">Web Design</p>
-  </div>
-  </div>
-  
 
-</div>
+           {project.map((i,item) => (
+               <div  key={i.id} className="w-full md:w-1/3 p-1">
+
+               <div className=" rounded-lg bg-white thrink-0 ">
+               <Image key={i.id} src={i.imag} alt="project"  width="99" height="33" className="w-[100%] p-2 md:hover:scale-95"/>
+               <div className="grid grid-cols-2">
+                <div>
+                <div className="align-content-between">
+                <p className="font-bold px-2">{i.title}</p>
+                <p className="text-slate-500 px-2">{i.subtitle}</p>
+                 </div>
+                </div>
+                <div>
+                    <p className="rounded-lg  cursor-pointer flex justify-end p-2 " onClick={handleProject}>
+                         <FaArrowAltCircleRight height="60" width="90"/>
+                    </p>
+                </div>
+               </div>
+              
+                {/* <div className="align-content-between bg-[#0788FF]">
+                <p className="font-bold px-2">{i.title}</p>
+                <p className="text-slate-500 px-2">{i.subtitle}</p>
+                 </div> */}
+               </div>
+              
+             </div>
+             
+           ))}
+          </div>
          
         </div>
     )
